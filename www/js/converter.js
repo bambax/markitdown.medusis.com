ae30a61bad34a0603524b73383e48ecb39fd6f06
Emmanuel Bégué <medusis+github@gmail.com>
2013-04-04 13:40:03 +0200

    clean2

--- www/js/converter.js
+++ www/js/converter.js
@@ -7,44 +7,16 @@ Mark It Down
 
 $(document).ready(function() {
 	/*
-	************************************************************************
-	********************************* UI ***********************************
-	************************************************************************	
-	*/
-
-	$("#footer a:not(.click)").live("click", function(e) {
-		var	target = this.hash.substr(1);
-		$("#content > div").fadeOut(0);
-		$("#content_" + target).fadeIn();
-		});
-	$("#content > div").live("click", function() {
-		$(this).fadeOut();
-		});
-	$("#content a").attr("target", "_blank").live("click", function(e) {
-		e.stopPropagation();
-		});
-
-	/*
-	************************************************************************
 	**************************** CONVERTER *********************************
-	************************************************************************	
 	*/
 
-	$("#buttonContainer").live("click", function(e) {
-		var	action = (e.srcElement && e.srcElement.name) || e.liveFired.activeElement.name;
-		if (action === "convert_html2mk") {
-			$("#convert_addDoc").attr("disabled", false);
-			}
-		});
 	$("#convert_raz").live("click", function() {
 		$("#converter_source, #converter_result").text("");
 		});
 
 
 	/*
-	************************************************************************
 	***************************** *UTILS ***********************************
-	************************************************************************	
 	*/
 
 	// remake of alert to prevent bizarre Saxon errors from jumping in front of the user
